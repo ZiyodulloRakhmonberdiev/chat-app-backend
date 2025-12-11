a72da7c91598e5e04370e2d1c12ca8d019b72039
Ziyodullo Rakhmonberdiev (Ali) <ziyodillo808@gmail.com>
2025-12-12 00:15:23 +0300

    created route: check auth route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -102,3 +102,12 @@ export const updateProfile = async (req, res) => {
     console.log("Error in update profile: ", error.message);
   }
 };
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
